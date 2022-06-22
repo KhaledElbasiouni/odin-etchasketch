2d9f264fcc41ece63028feb6c8c1d522a4f66a6e
KhaledElbasiouni <khaled.elbasiouni222@gmail.com>
2022-06-22 22:24:10 +0300

    Implement color picker

--- script.js
+++ script.js
@@ -2,11 +2,12 @@
 const DEFAULT_SIZE = 16;
 const CANVAS_COLOR = '#fff';
 const DEFAULT_MODE = 'pen';
+const DEFAULT_COLOR = '#000';
 let numSquares = DEFAULT_SIZE;
 
 const gridContainer = document.querySelector('.grid-container');
 
-let selectedColor = 'black';
+let selectedColor = DEFAULT_COLOR;
 let currMode = DEFAULT_MODE;
 createGrid();
 let gridItems = document.querySelectorAll('.grid-item');
@@ -19,6 +20,7 @@ const eraserBtn = document.querySelector('.eraser-btn');
 const clearBtn = document.querySelector('.clear-btn');
 const slider = document.querySelector('.slider');
 const value = document.querySelector('.value');
+const colorPicker = document.querySelector('.color-picker');
 
 setValue(DEFAULT_SIZE);
 let mouseDown = false;
@@ -28,7 +30,7 @@ penBtn.onclick = () => setCurrentMode('pen');
 rainbowBtn.onclick = () => setCurrentMode('rainbow');
 eraserBtn.onclick = () => setCurrentMode('eraser');
 clearBtn.onclick = () => clearGrid();
-
+colorPicker.onchange = (e) => setColor(e.target.value); 
 slider.onmousemove = (e) => {
     setValue(e.target.value);
 };
@@ -116,4 +118,8 @@ function activateButton(newMode) {
 
 function setValue(newValue){
     value.innerHTML = `${newValue} x ${newValue}`;
+}
+
+function setColor(newColor) {
+    selectedColor = newColor;
 }
